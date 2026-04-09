631ed1e5bb86955f9a8e37ecf16262454e30f88e
hide-on-smoke <maison.maithaison@gmail.com>
2026-04-09 20:12:04 +0700

    Revert "feat: complete Dashboard UI, navigation routing"
    This reverts commit 87e4833975cfa14b765a9f22cd8302f11cffc2f1.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Thêm dòng này
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Thêm dòng này vào danh sách plugins
-  ],
+  plugins: [react()],
 })
